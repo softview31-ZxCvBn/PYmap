e5744e61e66bdea61d08b6846c2646d4dc74cb1c
softview31-ZxCvBn <softview31@gmail.com>
2026-03-02 22:05:56 +0600

    Fix formatting and re-indent color dictionary

--- Python_Scanner.py
+++ Python_Scanner.py
@@ -1,8 +1,16 @@
 # Python-Port-Scanner-
-import socket                                 import subprocess                            color = {                                            "red": "\033[31m",
-"green": "\033[32m",                         "yellow": "\033[33m",                  "blue": "\033[34m",                     "magenta": "\033[35m",
-"cyan": "\033[36m",                   "white": "\033[37m",
-"bold": "\033[1m",                 "underline": "\033[4m",
+import socket
+import subprocess
+color = {
+"red": "\033[31m",
+"green": "\033[32m",
+"yellow": "\033[33m",
+"blue": "\033[34m",
+"magenta": "\033[35m",
+"cyan": "\033[36m",
+"white": "\033[37m",
+"bold": "\033[1m",
+"underline": "\033[4m",
 "reset": "\033[0m"
 }
 
